a49211ab34139d2b52cc4a040aaead71604e02be
Rostyslav Sapeliuk <rostyslav.sapeliuk@etiya.com>
2021-02-03 17:25:16 +0200

    Enabled polygon mode

--- src/main/java/engine/Window.java
+++ src/main/java/engine/Window.java
@@ -81,6 +81,7 @@ public class Window {
         glEnable(GL_DEPTH_TEST);
         glEnable(GL_BLEND);
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
 
     }
 
